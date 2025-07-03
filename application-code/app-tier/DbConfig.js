ed4baea7af76aa174490d1c3ddd8e8b88c7be7e8
anikethashok655 <anikethashok655@gmail.com>
2025-07-03 13:44:37 +0530

    DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -2,5 +2,5 @@ module.exports = Object.freeze({
     DB_HOST : 'database-1.cxqi0sa4mswn.ap-south-1.rds.amazonaws.com',
     DB_USER : 'admin',
     DB_PWD : 'aniketh1',
-    DB_DATABASE : ' webappdb'
+    DB_DATABASE : 'webappdb'
 });
